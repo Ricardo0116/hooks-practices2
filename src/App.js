94886dbbab1651227055406b87169a5b63368587
Ricardo0116 <rcapera16@gmail.com>
2024-06-12 22:19:34 -0500

    change in MyFirstComponent, counter useState, add componente SecondComponent

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
+import { MyFirstComponent } from './components/MyFirstComponent';
+
 
 function App() {
   return (
@@ -7,6 +9,11 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         
+        <h2>Hook useState</h2>
+        <div>
+          <hr />
+          <MyFirstComponent />
+        </div>
       </header>
     </div>
   );
